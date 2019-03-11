58a443cd1c80342e7f7c4f509a9e6c43ccfa9737
impadmin <impadmin@IMPETUS-1442>
2019-03-11 17:19:00 +0530

    Commit with multiple Files

---
+++ dummy3.java
@@ -0,0 +1,12 @@
+class dummy3 {
+  public static void main(String[] args) {
+    boolean learning = true;
+ 
+    if (learning) {
+      System.out.println("Dummy 3");
+    }
+    else {
+      System.out.println("What are you doing here?");
+    }
+  }
+}
---
+++ dummy4.java
@@ -0,0 +1,7 @@
+class dummy4 {
+  public static void main(String[] args) {
+    for (String t: args) {
+      System.out.println(t+ " Dummy 4");
+    }
+  }
+}
